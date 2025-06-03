cb940d28692dedaa78e49eb949f2544e941b149a
Zackyyhaa <zackyhafsa089@gmail.com>
2025-06-03 21:40:48 +0700

    styling message card

--- app/components/MessageCard.tsx
+++ app/components/MessageCard.tsx
@@ -6,8 +6,10 @@ type MessagePropsType = {
 export const MessageCard = (props: MessagePropsType) => {
   return (
     <div
-      className={`rounded-2xl px-6 text-lg shadow-md py-4 max-w-md w-fit whitespace-pre-line break-words ${
-        props.role === "user" ? "bg-red-700 text-white ml-auto" : "bg-white border"
+      className={` px-6 text-lg shadow-md py-4 max-w-md w-fit whitespace-pre-line break-words ${
+        props.role === "user"
+          ? "bg-red-700 text-white ml-auto rounded-b-2xl rounded-tl-2xl"
+          : "bg-white border rounded-b-2xl rounded-tr-2xl"
       }`}
     >
       {props.message}
